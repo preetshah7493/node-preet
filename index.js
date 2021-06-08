197aa602f7b37915020d4672c1a772366b49dc6e
preetshah7493 <preetshah7493@gmail.com>
2021-06-08 16:54:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
+var app = express();
+var cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.get("/", function (req, res) {
+  url = "https://www.60secondsnow.com/amphtml/gu";
+
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      var newsArray = [];
+
+      const post_length = $("div.post-container").children().length;
+
+      $("div.post-container").map((i, item) => {
+        let json = { heading: "", description: "", image: "" };
+
+        $(".post-container")
+          .eq(i)
+          .find(".article-content")
+          .find("h2")
+          .filter(function () {
+            var data = $(this);
+            heading = data.text();
+            json.heading = heading;
+          });
+
+        $(".post-container")
+          .eq(i)
+          .find(".article-desc")
+          .find("p")
+          .filter(function () {
+            var data = $(this);
+            description = data.text();
+            json.description = description;
+          });
+        $(".post-container")
+          .eq(i)
+          .find(".article-img")
+          .find("amp-img")
+          .filter(function () {
+            var data = $(this);
+            image = data.attr("src");
+            json.image = image;
+          });
+
+        newsArray.push(json);
+      });
+    }
+
+    res.send(newsArray);
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
